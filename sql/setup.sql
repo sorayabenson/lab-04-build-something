028031ba87b4e568331d288e85e930fa45142473
Soraya Benson <soraya.benson@gmail.com>
2021-03-31 19:03:49 -0700

    file tree updated to reflect new direction of the project

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,11 +3,26 @@ DROP TABLE IF EXISTS users, contacts;
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT NOT NULL,
-    hash TEXT NOT NULL
+    hash TEXT NOT NULL,
+    user_name TEXT NOT NULL
 );
-CREATE TABLE contacts (
+CREATE TABLE favorites (
+    id TEXT ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    images OBJECT NOT NULL,
+    slug TEXT NOT NULL,  
+    url TEXT NOT NULL,
+    bitly_url TEXT NOT NULL,
+    embed_url TEXT NOT NULL,
+    username TEXT NOT NULL,
+    source TEXT NOT NULL,
+    source_post_url TEXT NOT NULL,
+    rating TEXT NOT NULL,
+    collection BIGINT,    
+    user_id BIGINT NOT NULL
+);
+CREATE TABLE collections (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
-    phone_number TEXT NOT NULL,
     user_id BIGINT NOT NULL
 );
